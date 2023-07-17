342f82277023c8765f98627c03489a95eba7bbaf
Enton-72 <64578992+Enton-72@users.noreply.github.com>
2023-07-17 15:42:06 +0200

    Delete get-global-config.sh

--- ton-access/get-global-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# make directory if doesnt exist
-mkdir -p config
-# fetch
-curl -sL https://ton-blockchain.github.io/global.config.json > ./config/global-mainnet.json
-curl -sL https://ton-blockchain.github.io/testnet-global.config.json > ./config/global-testnet.json
