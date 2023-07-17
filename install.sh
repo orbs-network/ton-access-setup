8f42516d9882c4acf2a890853d18aff8f6d3c23d
Enton-72 <64578992+Enton-72@users.noreply.github.com>
2023-07-17 15:04:01 +0200

    Added logic for cron job

--- install.sh
+++ install.sh
@@ -1,28 +1,84 @@
 #!/bin/bash
-# Description: This script will prepare ton-access-nodes Git project for deployment of docker compose file.
+# Description: This script will prepare ton-access-setup Git project for deployment of docker containers.
 
+HOME_DIR="$HOME"
+SLACK_URL="" # Dev
+SLACK_URL="" # Prod
+SCRIPT_NAME=$(basename $0)
+TON_ACCESS_DIR=""
+
+send_slack () {
+	curl -s -X POST $SLACK_URL -H 'Content-type: application/json' --data '{"text":"'"$(hostname): $1"'"}' >/dev/null
+}
 eecho() {
 	echo $@ 1>&2
 	exit 1;
 }
+checkCronjob() {
+	crontab -l|grep '#'$REPO_NAME''
+}
+installAccessSetup() {
+	echo "Installing..."
+	echo -n "[1/3] Creating config directory... "
+	CONFIG_DIR="$REPO_DIR/ton-access/config"
+	[ -d $CONFIG_DIR ] && rm -rf $CONFIG_DIR
+	mkdir $CONFIG_DIR
+	[ ! -d "$CONFIG_DIR" ] && eecho "Failed!"
+	echo "Done"
+
+	echo -n "[2/3] Downloading global-mainnet.json and global-testnet.json... "
+	curl -sL https://ton-blockchain.github.io/global.config.json > $CONFIG_DIR/global-mainnet.json; [ $? -gt 0 ] && eecho "Error occurred while trying to download global-mainnet.json file!";
+	curl -sL https://ton-blockchain.github.io/testnet-global.config.json > $CONFIG_DIR/global-testnet.json; [ $? -gt 0 ] && eecho "Error occurred while trying to download global-testnet.json file!";
+	echo "Done"
+
+	echo -n "[3/3] Copying ton-access to $HOME_DIR... "
+	cp -r ./ton-access $HOME_DIR ; [ $? -gt 0 ] && eecho "ton-access directory copy failed!";
+	TON_ACCESS_DIR="$HOME_DIR/ton-access"
+	echo "Done"
+	rm -rf $CONFIG_DIR
+	echo "Done"
+}
 
 [[ ! $(id -un) == "ubuntu" ]] && eecho "Execute this script as \"ubuntu\" user.";
 [ -z $(find . -type d -name ".git") ] && eecho "Execute this script inside of Git project."
-MAIN_DIR="$HOME"
+APPS="git python3 curl crontab"
+for i in $APPS
+do
+	command -v $i > /dev/null 2>&1
+	if [ ! $? -eq 0 ]
+	then
+		eecho "$i Not found! Please install this app first."
+	fi
+done
+REPO_DIR=$(git rev-parse --show-toplevel); [ -z "$REPO_DIR" ] && eecho "Unable to get full path of repository!"
+REPO_NAME="$(basename $REPO_DIR)"; [ -z "$REPO_NAME" ] && eecho "Unable to get name of repository!"
 
-echo -n "[1/3] Creating config directory... "
-CONFIG_DIR="$([ -d config ] && rm -rf config; mkdir -v config)"
-[ ! -d $CONFIG_DIR ] && eecho "Failed!"
-echo "Done"
-
-echo -n "[2/3] Downloading global-mainnet.json and global-testnet.json... "
-curl -sL https://ton-blockchain.github.io/global.config.json > $CONFIG_DIR/global-mainnet.json; [ $? -gt 0 ] && eecho "Error occurred while trying to download global-mainnet.json file!";
-curl -sL https://ton-blockchain.github.io/testnet-global.config.json > $CONFIG_DIR/global-testnet.json; [ $? -gt 0 ] && eecho "Error occurred while trying to download global-testnet.json file!";
-echo "Done"
-
-echo -n "[3/3] Copying ton-access to $MAIN_DIR... "
-cp -r ./ton-access $MAIN_DIR ; [ $? -gt 0 ] && eecho "ton-access directory copy failed!";
-echo "Done"
+### Main
+cd $REPO_DIR
+if [ $# -eq 0 ]
+then
+	installAccessSetup
+	### Cron job installation
+	JOB=$(checkCronjob)
+	if [ -z "$JOB" ]; then
+		echo -n "Installing cron job... "
+		(crontab -l; echo -e '* * * * * /bin/bash -c '$HOME_DIR'/'$REPO_NAME'/install.sh --cron;#'$REPO_NAME'') | crontab -
+		JOB=$(checkCronjob)
+		[ -z "$JOB" ] && eecho "Failed to install cron job!"
+		echo "Done"
+	fi
+elif [ "$1" == "--cron" ]
+then
+	FETCH=$(git fetch --dry-run)
+	[ -z "$FETCH" ] && exit 0;
+	PULL=$(git pull)
+	[[ $PULL =~ "Already up to date" ]] && send_slack "Nothing to pull from git!" && exit 0
+	STATUS=$(git status --porcelain)
+	[ ! -z "$STATUS" ] && send_slack "git status: $STATUS!"
+	installAccessSetup
+else
+	eecho "Unknown variable!"
+fi
 
 # Stop further execution
 exit $?
@@ -33,6 +89,6 @@ python3 /usr/src/mytonctrl/mytoninstaller.py <<< clcf >/dev/null
 [ $? -gt 0 ] && eecho "Failed!"
 echo "Done"
 
-echo "[5/?] Executing docker compose up -d in $MAIN_DIR/ton-access as root... "
+echo "[5/?] Executing docker compose up -d in $HOME_DIR/ton-access as root... "
 cd $TON_ACCESS_DIR && sudo docker compose up -d
 [ $? -eq 0 ] && echo "Removing $GIT_PROJECT_DIR..." && rm -rf $GIT_PROJECT_DIR && echo "Done"
